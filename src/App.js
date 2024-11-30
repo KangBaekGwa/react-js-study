e2149b3d7c8184ea018d60f29e2d3f4a37211b57
KangBaekGwa <ksu9801@gmail.com>
2024-11-30 18:01:01 +0900

    홀짝카운터

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-4xl font-bold text-center">{number}</h1>
+      <h1 className="texs-2xl text-center mt-2">
+        {number % 2 === 0 ? "짝수" : "홀수"}
+      </h1>
+      <button
+        className="mt-4 px-6 py-2 bg-blue 500 text-black rounded hover:bg-blue-700 transition-colors"
+        onClick={() => setNumber(number + 1)}
+      >
+        증가
+      </button>
+    </>
   );
 }
 
